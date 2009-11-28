3c3197d2325b1fdc252db4bfe1829c9505b30e70
Ryan Bigg <radarlistener@gmail.com>
2009-11-28 21:51:31 +1000

    Remove g

--- lib/summer.rb
+++ lib/summer.rb
@@ -68,7 +68,7 @@ module Summer
         message = words[3..-1].join(" ").gsub(/^:/, '')
         # Parse commands
         if /^!(\w+)\s*(.*)/.match(message) && respond_to?("#{$1}_command")
-g          try("#{$1}_command", parse_sender(sender), channel, $2)
+          try("#{$1}_command", parse_sender(sender), channel, $2)
         # Plain and boring message
         else
           method = channel == me ? :did_receive_private_message : :did_receive_channel_message
